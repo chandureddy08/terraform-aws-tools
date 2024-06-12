dd791b588828c490fe8ddcdf5ff077fc978e483b
chandureddy08 <chandrareddyerigela@gmail.com>
2024-06-12 14:33:53 -0400

    cicd dev infra

--- cicd/main.tf
+++ cicd/main.tf
@@ -33,7 +33,7 @@ resource "aws_key_pair" "tools" {
   key_name   = "tools"
   # you can paste the public key directly like this
   #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ONJth+DzeXbU3oGATxjVmoRjPepdl7sBuPzzQT2Nc sivak@BOOK-I6CR3LQ85Q"
-  public_key = file("~/.ssh/vpn.pub")
+  public_key = file("~/.ssh/tools.pub")
   # ~ means windows home directory
 }
 
@@ -47,7 +47,7 @@ module "nexus" {
   # convert StringList to list and get first element
   subnet_id = "subnet-02438e2af84b2481e"
   ami = data.aws_ami.nexus_ami_info.id
-  key_name = aws_key_pair.vpn.key_name
+  key_name = aws_key_pair.tools.key_name
   root_block_device = [
     {
       volume_type = "gp3"
